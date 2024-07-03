454ea53f8ab7a6b57a99e0d23dc8dfe7d690fb36
Govind-05 <govindbhardwaj501@gmail.com>
2024-07-03 22:15:09 +0530

    Server Setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express')
+
+const app=express();
+
+const PORT=5000
+
+app.get("/",(req,res)=>{
+    res.send("SERVER 123 IS RUNNING")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`);
+})

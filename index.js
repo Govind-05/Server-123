4e953065f3c243d1512269f82db0e6f20725e1a2
Govind Bhardwaj <91485828+Govind-05@users.noreply.github.com>
2024-07-06 18:12:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get("/",(req,res)=>{
     res.send("SERVER 123 IS RUNNING")
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, '0.0.0.0', ()=>{
     console.log(`Server is listening on port ${PORT}`);
 })
